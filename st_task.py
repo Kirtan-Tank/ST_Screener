91a96987c27d07b1f5d728905d3b85c9b881ac21
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2023-10-16 00:57:10 +0530

    Update st_task.py

--- st_task.py
+++ st_task.py
@@ -22,12 +22,12 @@ ticker_val = st.text_input("Enter the stock ticker (e.g., AAPL for Apple):")
 if ticker_val:
     ticker = yf.Ticker(ticker_val)
     st.subheader(f"Information about {ticker_val}")
-    st.dataframe(pd.DataFrame(ticker.info))
+    # st.dataframe(pd.DataFrame(ticker.info))
     # st.DataFrame(ticker.info).iloc[0, 0:8]
 
     # Load stock splits, dividends, major holders, institutional holders, and cashflow data
     st.subheader("Additional Information")
-    # st.dataframe(ticker.splits)
+    st.dataframe(ticker.splits)
     # st.dataframe(ticker.dividends)
     # st.dataframe(ticker.major_holders)
     # st.dataframe(ticker.institutional_holders)
