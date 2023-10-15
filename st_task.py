2dc59c9eb84b4450c51c05410c33a1a04667b573
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2023-10-16 00:35:00 +0530

    Update st_task.py

--- st_task.py
+++ st_task.py
@@ -25,12 +25,12 @@ if ticker_val:
     st.dataframe(pd.DataFrame(ticker.info).iloc[0, 0:8])
 
     # Load stock splits, dividends, major holders, institutional holders, and cashflow data
-    st.subheader("Additional Information")
-    st.dataframe(ticker.splits)
-    st.dataframe(ticker.dividends)
-    st.dataframe(ticker.major_holders)
-    st.dataframe(ticker.institutional_holders)
-    st.dataframe(ticker.cashflow)
+    # st.subheader("Additional Information")
+    # st.dataframe(ticker.splits)
+    # st.dataframe(ticker.dividends)
+    # st.dataframe(ticker.major_holders)
+    # st.dataframe(ticker.institutional_holders)
+    # st.dataframe(ticker.cashflow)
 
     # Get today's date
     today = datetime.now().date().strftime("%Y-%m-%d")
