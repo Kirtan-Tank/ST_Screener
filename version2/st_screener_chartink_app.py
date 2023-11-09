97bd46737c76b19a07d34466bc69736604045d0a
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2023-11-09 21:14:53 +0530

    Update st_screener_chartink_app.py

--- version2/st_screener_chartink_app.py
+++ version2/st_screener_chartink_app.py
@@ -4,7 +4,7 @@ import requests
 import lxml
 import streamlit as st
 import json
-
+from streamlit_lottie import st_lottie
 from bs4 import BeautifulSoup as bs
 
 url= "https://chartink.com/screener/process"
@@ -12,6 +12,15 @@ url= "https://chartink.com/screener/process"
 # Title and header
 st.title("Stock Screener App v2 📈")
 
+def lottieurl_load(url: str):
+    r= requests.get(url)
+    if r.status_code !=200:
+        return None
+    return r.json()
+    
+lottie_img = lottieurl_load("https://lottie.host/b9c12781-35cd-4369-a17e-2f74f8147a23/vPpe1iW4uP.json")   
+with st.columns(3)[1]:
+    st_lottie(lottie_img,speed=1,reverse=False,loop=True,quality="high",height=350,width=350,key=None)
 
 with requests.session() as s:
   raw_data= s.get(url)
